dec031a9c314959e49bb6c080adf1a36826beba9
Amy Luo <amy.luo.939@gmail.com>
2018-05-29 13:50:14 -0400

    hamburger nav

--- functions.js
+++ functions.js
@@ -2,3 +2,12 @@ $(window).scroll(function() {
     var h = $(".titleBox").height();
     $(".titleBox").css("opacity", 1 - $(window).scrollTop() / h);
   });
+
+function myFunction() {
+    let x = document.getElementById("navBar");
+    if (x.className === "navBar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navBar";
+    }
+}
