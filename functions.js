7ec3817e536777fbc3189764ae6099900c34098d
Amy Luo <amy.luo.939@gmail.com>
2018-06-14 15:09:10 -0400

    art modal

--- functions.js
+++ functions.js
@@ -19,4 +19,42 @@ $(document).ready(function(){
         $("#artBtn").css({"color": "black", "font-weight": "500"});
         $("#projectBtn").css({"color": "gray", "font-weight": "normal"});
     });
+
+
+    // Get the modal
+    const modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+    const artThumbnails = document.getElementsByClassName("art");
+    const modalImg = document.getElementById("modalImg");
+    const captionText = document.getElementById("caption");
+
+
+    for (let i = 0, len = artThumbnails.length; i < len; i++) {
+        const artThumb = artThumbnails[i];
+
+        artThumb.onclick = function() {
+            modal.style.display = "block";
+            style = artThumb.currentStyle || window.getComputedStyle(artThumb, false);
+
+            modalImg.src = style.backgroundImage.slice(4, -1).replace(/"/g, "");
+            captionText.innerHTML = artThumb.childNodes[5].innerHTML;
+        }
+    }
+
+
+// Get the <span> element that closes the modal
+    const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    };
+
+    modal.onclick = function() {
+        modal.style.display = "none";
+    };
 });
+
+
+
