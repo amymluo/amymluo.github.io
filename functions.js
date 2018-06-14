25110c03ce4127f8a3a04e80441087fbca893aa1
Amy Luo <amy.luo.939@gmail.com>
2018-06-14 13:55:51 -0400

    art thumbnails

--- functions.js
+++ functions.js
@@ -1,14 +1,22 @@
-$(window).scroll(function() {
+$(window).scroll(function () {
     const box = $(".titleBox");
     const h = box.height();
     box.css("opacity", 1 - $(window).scrollTop() / h);
-  });
+});
 
-function myFunction() {
-    let x = document.getElementById("navBar");
-    if (x.className === "navBar") {
-        x.className += " responsive";
-    } else {
-        x.className = "navBar";
-    }
-}
+$(document).ready(function(){
+    $("#projectBtn").click(function(){
+        console.log("here");
+        $("#art").fadeOut(600);
+        $("#projects").fadeIn(600);
+        $("#projectBtn").css({"color": "black", "font-weight": "500"});
+        $("#artBtn").css({"color": "gray", "font-weight": "normal"});
+    });
+
+    $("#artBtn").click(function(){
+        $("#projects").fadeOut(600);
+        $("#art").fadeIn(600);
+        $("#artBtn").css({"color": "black", "font-weight": "500"});
+        $("#projectBtn").css({"color": "gray", "font-weight": "normal"});
+    });
+});
